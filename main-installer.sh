eb57313a80b7c039cc6a73b880b1cf9b4a2624c7
opchauhan87 <opendra.s.chauhan@gmail.com>
2026-08-14 16:20:41 +0530

    Update main-installer.sh
    add directory hncsmart-alama

--- main-installer.sh
+++ main-installer.sh
@@ -196,7 +196,7 @@ echo "Install Perl"
 yum install -y perl-CPAN perl-YAML perl-CPAN-DistnameInfo perl-libwww-perl perl-DBI perl-DBD-MySQL perl-GD perl-Env perl-Term-ReadLine-Gnu perl-SelfLoader perl-open.noarch 
 
 #CPM install
-cd /usr/src/vicidial-install-scripts
+cd /usr/src/hncsmart-alama
 curl -fsSL https://raw.githubusercontent.com/skaji/cpm/main/cpm | perl - install -g App::cpm
 /usr/local/bin/cpm install -g
 
